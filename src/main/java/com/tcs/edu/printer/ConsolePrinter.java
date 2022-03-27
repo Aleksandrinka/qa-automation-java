2f486c256460176a2042a45c95b1a83fcb067628
Alexandrina Chvarova <lxndrnrrdk@gmail.com>
2022-03-27 21:55:07 +0300

    feat: Добавлен класс TimestampMessageDecorator для инструментов модификации сообщений feat: Добавлен метод TimestampMessageDecorator.decorate для добавления текущих даты и времени к сообщению docs: Добавлены javadoc комментарии к классу TimestampMessageDecorator и его методу decorate

--- src/main/java/com/tcs/edu/printer/ConsolePrinter.java
+++ src/main/java/com/tcs/edu/printer/ConsolePrinter.java
@@ -2,12 +2,14 @@ package com.tcs.edu.printer;
 
 /**
  * Класс для работы с выводом в консоль
+ *
  * @author a.chvarova
  */
 
 public class ConsolePrinter {
     /**
      * Метод, выводящий строку в консоль
+     *
      * @param message сообщение для вывода в консоль
      */
     public static void print(String message) {
