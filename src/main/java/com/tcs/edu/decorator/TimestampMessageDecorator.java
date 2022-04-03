70559ca36b80e868169f4be09c495130cc1d95e1
Alexandrina Chvarova <lxndrnrrdk@gmail.com>
2022-04-03 21:55:19 +0300

    Revert "feat: Изменено объявление поясняющей переменной decoratedMessage, добавлен модификатор final."
    This reverts commit c1b9c295

--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -18,7 +18,7 @@ public class TimestampMessageDecorator {
      * @return к полученной на вход строке добавляется счетчик выведенных сообщений, текущая дата и время
      */
     public static String decorate(String message) {
-        final var decoratedMessage = messageCount.toString() + " " + Instant.now() + " " + message;
+        var decoratedMessage = messageCount.toString() + " " + Instant.now() + " " + message;
         return decoratedMessage;
     }
 }
