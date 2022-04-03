59331cf0f3e5d01784848b40a6fedc16cb0f2c27
Alexandrina Chvarova <lxndrnrrdk@gmail.com>
2022-04-03 21:31:58 +0300

    feat:  Добавлено глобальное состояние приложения: общая переменная с количеством выведенных сообщений. Значение счётчика добавлено в вывод. docs: Дополнены комментарии в связи с изменениями.

--- src/main/java/com/tcs/edu/printer/ConsolePrinter.java
+++ src/main/java/com/tcs/edu/printer/ConsolePrinter.java
@@ -1,5 +1,7 @@
 package com.tcs.edu.printer;
 
+import com.tcs.edu.decorator.TimestampMessageDecorator;
+
 /**
  * Класс для работы с выводом в консоль
  *
@@ -9,10 +11,12 @@ package com.tcs.edu.printer;
 public class ConsolePrinter {
     /**
      * Метод, выводящий строку в консоль
+     * Вызов метода приведет к изменению значения глобальной переменной messageCount
      *
      * @param message сообщение для вывода в консоль
      */
     public static void print(String message) {
-        System.out.println(message);
+        TimestampMessageDecorator.messageCount++;
+        System.out.println(TimestampMessageDecorator.decorate(message));
     }
 }
